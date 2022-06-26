ff7a6644cd8aab8b14ef7540efafa42d46c2685a
Mountain <blckclst@gmail.com>
2022-06-26 19:17:53 +0500

    Добавил один тест для второй задачи

--- homework-1.py
+++ homework-1.py
@@ -34,6 +34,7 @@ def int32_to_ip(int32):
 
 assert int32_to_ip(2154959208) == "128.114.17.104"
 assert int32_to_ip(0) == "0.0.0.0"
+assert int32_to_ip(32) == "0.0.0.32"
 assert int32_to_ip(2149583361) == "128.32.10.1"
 
 
