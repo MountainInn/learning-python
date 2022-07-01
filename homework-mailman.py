c4ec925de8d988066573f1b94c846dac40348e09
Mountain <blckclst@gmail.com>
2022-07-01 16:08:08 +0500

    Переписал класс Path и удалил ненужные классы

--- homework-mailman.py
+++ homework-mailman.py
@@ -14,37 +14,36 @@ points = [
 
 possible_paths = list( permutations(points) )
 
-print(f"Permutations count: {len(possible_paths)}")
-
-Point = tuple[ int, int ]
-
-@dataclass
-class Segment:
-    start_point : Point
-    end_point : Point
-    distance: float
-
 class Path:
-    def __init__(self):
-        self.segments = list()
-        self.total_distance = 0
+    def __init__(self, starting_point):
+        self.starting_point = starting_point
+        self.points = list()
+        self.distances = list()
+
+    def append(self, point : Point, distance : float):
+        self.points.append(point)
+
+        if self.distances:
+            distance += self.distances[-1]
+
+        self.distances.append(distance)
+
+    def get_total_distance(self):
+        return self.distances[-1] if self.distances else 0
 
     def __repr__(self):
-        start_point = self.segments[0].start_point
-        distance = 0
-        steps = "".join([f" -> {s.end_point}[{s.distance}]" for s in self.segments ])
-        return f'{start_point}{steps} = {self.total_distance}'
+        steps = "".join([f" -> {self.points[i]}[{self.distances[i]}]" for i in range(len( self.points )) ])
+        return f'{self.starting_point}{steps} = {self.get_total_distance()}'
 
 results : List[Path] = list()
 
 for path in possible_paths:
     current_point = start
-    result_path : Path = Path()
+    result_path : Path = Path(start)
 
     for next_point in points:
         distance = dist(current_point, next_point)
-        result_path.segments.append(Segment(current_point, next_point, distance))
-        result_path.total_distance += distance
+        result_path.append(next_point, distance)
         current_point = next_point
 
     results.append(result_path)
@@ -53,7 +52,7 @@ min_distance = float_info.max
 min_index = -1
 
 for index , res in enumerate( results ):
-    if res.total_distance < min_distance:
+    if res.get_total_distance() < min_distance:
         min_index = index
 
 shortest_path = results[min_index]
