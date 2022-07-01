7846e50aa6a4fc1d9ef61050c699d24b37a23dd3
Mountain <blckclst@gmail.com>
2022-07-01 12:45:15 +0500

    Добавил заготовку под задачу 2.2

--- homework-2.py
+++ homework-2.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import Generator, List, Tuple, Iterable, Iterator
 
 # Задача 1
 
@@ -29,3 +32,21 @@ for i in cyclic_iterator:
     if iteration_count >= break_after:
         break
 
+# Задача 2
+
+@dataclass
+class Movie:
+    title: str
+    dates: List[Tuple[datetime, datetime]]
+
+    def schedule(self) -> Generator[datetime, None, None]:
+        return []
+
+
+m = Movie('sw', [
+  (datetime(2020, 1, 1), datetime(2020, 1, 7)),
+  (datetime(2020, 1, 15), datetime(2020, 2, 7))
+])
+
+for d in m.schedule():
+    print(d)
