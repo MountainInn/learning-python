29528e95cd23f74c99525b820e9990565f64e990
Mountain <blckclst@gmail.com>
2022-07-02 14:58:42 +0500

    Перенес вычисление дистанций в конструктор Path

--- homework-mailman.py
+++ homework-mailman.py
@@ -13,16 +13,27 @@ points = [
 points_permutations = list( permutations(points) )
 
 class Path:
-    def __init__(self, starting_point):
-        self.starting_point = starting_point
+    def __init__(self, starting_point, other_points):
         self.points = list()
         self.distances = list()
+        self.starting_point = starting_point
+
+        current_point = starting_point
+
+        for next_point in other_points:
+            distance = dist(current_point, next_point) + self.get_total_distance()
+
+            self.distances.append(distance)
+            self.points.append(next_point)
+
+            current_point = next_point
 
 
-    def append(self, point : tuple[ int, int ], distance : float):
-        distance += self.get_total_distance()
-        self.distances.append(distance)
-        self.points.append(point)
+    def __repr__(self):
+        points_range = range(len( self.points ))
+        steps = [f' -> {self.points[i]}[{self.distances[i]}]' for i in points_range]
+        steps_string = "".join(steps)
+        return f'{self.starting_point}{steps_string} = {self.get_total_distance()}'
 
     def get_total_distance(self):
         return self.distances[-1] if self.distances else 0
@@ -30,23 +41,9 @@ class Path:
 
 paths = list()
 
-for perm in points_permutations:
-    current_point = start
-    path : Path = Path(start)
-    def __repr__(self):
-        points_range = range(len( self.points ))
-        steps = [f' -> {self.points[i]}[{self.distances[i]}]' for i in points_range]
-        steps_string = "".join(steps)
-        return f'{self.starting_point}{steps_string} = {self.get_total_distance()}'
-
-    for next_point in perm:
-        distance = dist(current_point, next_point)
-        path.append(next_point, distance)
-        current_point = next_point
-
+for points_perm in points_permutations:
+    path : Path = Path(start, points_perm)
     paths.append(path)
-
-
 
 shortest_path = min(enumerate(paths), key=lambda pair: pair[1].get_total_distance())[1]
 
