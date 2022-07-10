c7fdd2d9682d925bb2f67464aba2fcc399533a5a
Mountain <blckclst@gmail.com>
2022-07-10 14:19:51 +0500

    Выполнил задачу 3.2

--- homework-3.py
+++ homework-3.py
@@ -1,4 +1,6 @@
 import itertools
+from abc import ABC, abstractmethod
+import time
 import redis
 
 #
@@ -86,6 +88,45 @@ def multiplier_with_dict(number: int):
 @cache_result(redis_cache)
 def multiplier_with_redis(number: int):
     return multiplier(number)
+
+#
+# Задача 3.2
+#
+
+
+def repeat(call_count: int, start_sleep_time: float, factor: float, border_sleep_time: float):
+    def decorator(func):
+
+        def output(repeat_count: int, sleep_time: float, result: float):
+            repeat_count_str: str = str(repeat_count+1).rjust(3, '-')
+            sleep_time_str: str = str(sleep_time).rjust(3, '-')
+            print(
+                f'Запуск номер {repeat_count_str}. Ожидание {sleep_time_str} секунд. Результат декорируемой функции = {result}')
+
+        def wrapper(*args, **kwargs):
+            repeat_count: int = 0
+            sleep_time: float = start_sleep_time
+
+            while repeat_count < call_count:
+                time.sleep(sleep_time)
+
+                result = func(*args, **kwargs)
+                output(repeat_count, sleep_time, result)
+
+                sleep_time = min(sleep_time + start_sleep_time *
+                                 2 ** factor, border_sleep_time)
+                repeat_count += 1
+
+        return wrapper
+
+    return decorator
+
+
+@repeat(call_count=6, start_sleep_time=1, factor=2, border_sleep_time=30)
+def repeated_multiplier(number: int):
+    return multiplier(number)
+
+
 def main():
     redis_cache.flushdb()
 
@@ -108,6 +149,9 @@ def main():
 
     multiplier_with_redis(9)
     multiplier_with_redis(9)
+
+    print("\nДекоратор repeat:")
+    repeated_multiplier(8)
 
 
 if __name__ == "__main__":
